b0dc966b47a47b2540e7d6497c81f6897d8366e2
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2023-08-14 18:18:32 +0800

    commit 3

--- TestTask/src/Main.java
+++ TestTask/src/Main.java
@@ -1,67 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-<<<<<<< HEAD
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-// Создаем файлы с данными и указываем путь
-        File file1 = new File("C:\\JavaProjects\\TestTask\\TestTask\\src\\Resources\\in1.txt");
-        File file2 = new File("C:\\JavaProjects\\TestTask\\TestTask\\src\\Resources\\in2.txt");
-        File file3 = new File("C:\\JavaProjects\\TestTask\\TestTask\\src\\Resources\\in3.txt");
-        File file4 = new File("C:\\JavaProjects\\TestTask\\TestTask\\src\\Resources\\out.txt");
-        merge(file1, file2, file3, file4);
-    }
-
-    public static void merge(File file1, File file2, File file3, File file4) {
-        List<Integer> numbers = new ArrayList<>();
-
-        Scanner scanner1 = null;
-        try {
-            scanner1 = new Scanner(file1);
-        } catch (FileNotFoundException e) {
-            System.out.println("File1 not found");
-        }
-        Scanner scanner2 = null;
-        try {
-            scanner2 = new Scanner(file2);
-        } catch (FileNotFoundException e) {
-            System.out.println("file2 not found");
-        }
-        Scanner scanner3 = null;
-        try {
-            scanner3 = new Scanner(file3);
-        } catch (FileNotFoundException e) {
-            System.out.println("file3 not found");
-        }
-
-        while (scanner1.hasNext() || scanner2.hasNext() || scanner3.hasNext()) {
-            numbers.add(scanner1.nextInt());
-            numbers.add(scanner2.nextInt());
-            numbers.add(scanner3.nextInt());
-
-        }
-        System.out.println(numbers);
-    }
-
-    public void divide(int startIndex,int endIndex){
-
-        //Divide till you breakdown your list to single element
-        if(startIndex<endIndex && (endIndex-startIndex)>=1){
-            int mid = (endIndex + startIndex)/2;
-            divide(startIndex, mid);
-            divide(mid+1, endIndex);
-
-            //merging Sorted array produce above into one sorted array
-            merger(startIndex,mid,endIndex);
-        }
-    }
-}
-=======
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -75,12 +14,12 @@ public class Main {
         File file1 = new File("C:\\Projects\\TestTask\\TestTask\\src\\Resources\\in1.txt");
         File file2 = new File("C:\\Projects\\TestTask\\TestTask\\src\\Resources\\in2.txt");
         File file3 = new File("C:\\Projects\\TestTask\\TestTask\\src\\Resources\\in3.txt");
-        File file4 = new File("C:C:\\Projects\\TestTask\\TestTask\\src\\Resources\\out.txt");
-        readFiles(file1, file2, file3);
+        File file4 = new File("C:\\Projects\\TestTask\\TestTask\\src\\Resources\\out.txt");
+        MergeAndWrite(file1, file2, file3, file4);
 
     }
 
-    public static void readFiles(File file1, File file2, File file3) {
+    public static void MergeAndWrite(File file1, File file2, File file3, File file4) {
         List<Integer> numbers1 = new ArrayList<>();
         List<Integer> numbers2 = new ArrayList<>();
         List<Integer> numbers3 = new ArrayList<>();
@@ -133,9 +72,16 @@ public class Main {
                     ? in2[j++]
                     : in3[k++]);
         }
-        System.out.println(Arrays.toString(out));
+   //     System.out.println(Arrays.toString(out));
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(file4);
+        } catch (FileNotFoundException e) {
+            System.out.println("File out not found");
+        }
+        pw.println(Arrays.toString(out));
+        pw.close();
     }
 }
 
 
->>>>>>> 3e3dcaf (Initial commit)
