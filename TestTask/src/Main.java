a69d4b762945e52ea1ec4b6dfe584de08226de5d
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2023-08-16 14:24:22 +0800

    commit 4

--- TestTask/src/Main.java
+++ TestTask/src/Main.java
@@ -1,12 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-import static java.lang.System.arraycopy;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,11 +10,28 @@ public class Main {
         File file2 = new File("C:\\Projects\\TestTask\\TestTask\\src\\Resources\\in2.txt");
         File file3 = new File("C:\\Projects\\TestTask\\TestTask\\src\\Resources\\in3.txt");
         File file4 = new File("C:\\Projects\\TestTask\\TestTask\\src\\Resources\\out.txt");
-        MergeAndWrite(file1, file2, file3, file4);
+        File fileSt1 = new File("FileStrings1");
+        File fileSt2 = new File("FileStrings2");
+        File fileSt3 = new File("FileStrings3");
 
+// Ввод клавиатуры в консоль
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите тип данных для сортировки (s - строчный, i - целочисленный)");
+        String x = null;
+        x = scanner.nextLine();
+        while (!Objects.equals(x, "s") && !Objects.equals(x, "i")) {
+            System.out.println("Вы ввели недопустимый символ, необходимо ввести s или i");
+            System.out.println("Введите тип данных для сортировки (s - строчный, i - целочисленный)");
+            x = scanner.nextLine();
+        }
+            if (x.equals("s"))
+                MergeAndWriteStrings(fileSt1, fileSt2, fileSt3, file4);
+            else if (x.equals("i"))
+                MergeAndWriteInt(file1, file2, file3, file4);
+        System.out.println("Отсортированные данные находятся в файле out.txt");
     }
-
-    public static void MergeAndWrite(File file1, File file2, File file3, File file4) {
+    // Метод объединяет и сортирует целочисленные значения
+    public static void MergeAndWriteInt(File file1, File file2, File file3, File file4) {
         List<Integer> numbers1 = new ArrayList<>();
         List<Integer> numbers2 = new ArrayList<>();
         List<Integer> numbers3 = new ArrayList<>();
@@ -34,31 +46,28 @@ public class Main {
             numbers1.add(scanner.nextInt());
         scanner.close();
         Integer[] in1 = numbers1.toArray(new Integer[numbers1.size()]);
-        System.out.println(Arrays.toString(in1));
         scanner.close();
 
         try {
             scanner = new Scanner(file2);
         } catch (FileNotFoundException e) {
-            System.out.println("File1 not found");
+            System.out.println("File2 not found");
         }
         while (scanner.hasNext())
             numbers2.add(scanner.nextInt());
         scanner.close();
         Integer[] in2 = numbers2.toArray(new Integer[numbers2.size()]);
-        System.out.println(Arrays.toString(in2));
         scanner.close();
 
         try {
             scanner = new Scanner(file3);
         } catch (FileNotFoundException e) {
-            System.out.println("File1 not found");
+            System.out.println("File3 not found");
         }
         while (scanner.hasNext())
             numbers3.add(scanner.nextInt());
         scanner.close();
         Integer[] in3 = numbers3.toArray(new Integer[numbers3.size()]);
-        System.out.println(Arrays.toString(in3));
         scanner.close();
 
         Integer[] out = new Integer[in1.length + in2.length + in3.length];
@@ -72,7 +81,7 @@ public class Main {
                     ? in2[j++]
                     : in3[k++]);
         }
-   //     System.out.println(Arrays.toString(out));
+        System.out.println(Arrays.toString(out));
         PrintWriter pw = null;
         try {
             pw = new PrintWriter(file4);
@@ -80,6 +89,61 @@ public class Main {
             System.out.println("File out not found");
         }
         pw.println(Arrays.toString(out));
+        pw.close();
+    }
+
+
+// Метод объединяет и сортирует строчные значения
+    public static void MergeAndWriteStrings(File fileSt1, File fileSt2, File fileSt3, File out){
+        List<String> strings1 = new ArrayList<>();
+        List<String> strings2 = new ArrayList<>();
+        List<String> strings3 = new ArrayList<>();
+        List<String> outStrings = new ArrayList<>();
+
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(fileSt1);
+        } catch (FileNotFoundException e) {
+            System.out.println("FileStrings1 not found");
+        }
+        while (scanner.hasNext())
+            strings1.add(scanner.nextLine());
+        scanner.close();
+
+        try {
+            scanner = new Scanner(fileSt2);
+        } catch (FileNotFoundException e) {
+            System.out.println("FileStrings2 not found");
+        }
+        while (scanner.hasNext())
+            strings2.add(scanner.nextLine());
+        scanner.close();
+
+
+        try {
+            scanner = new Scanner(fileSt3);
+        } catch (FileNotFoundException e) {
+            System.out.println("FileStrings3 not found");
+        }
+        while (scanner.hasNext())
+            strings3.add(scanner.nextLine());
+        scanner.close();
+
+        outStrings.addAll(strings1);
+        outStrings.addAll(strings2);
+        outStrings.addAll(strings3);
+        Collections.sort(outStrings);
+
+
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(out);
+        } catch (FileNotFoundException e) {
+            System.out.println("File out not found");
+        }
+        for(String str: outStrings)
+        pw.println(str + System.lineSeparator());
+        System.out.println(outStrings);
         pw.close();
     }
 }
